65aac69b6e43bc6539e6474d7c0909319a8cfc8e
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2016-11-09 22:52:01 +0200

    Set env process port

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var express = require('express');
 var app = express();
 
 app.use('/', express.static('.'));
-app.listen(8000, function() {
-    console.log('listening on port 8000');
+app.listen(process.env.PORT || 8000, function() {
+    console.log('listening on port ' + process.env.PORT || 8000);
 });
