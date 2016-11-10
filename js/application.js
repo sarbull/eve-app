3942e601f7637c570feb664a066d8206fa8c6d95
costin <costinaldea@yahoo.co.uk>
2016-11-10 02:26:49 +0200

    text to speech

--- js/application.js
+++ js/application.js
@@ -1,58 +1,62 @@
-(function() {
-  'use strict';
+(function () {
+    'use strict';
 
-  angular
-    .module('eveApp', [])
-    .controller('WelcomeController', ['$scope', function($scope) {
-      $scope.message = 'Welcome';
-    }])
-    .controller('ChatController', ['$scope', 'ApiAIService', function($scope, ApiAIService) {
-      $scope.input = '';
-      $scope.messages = [];
+    angular
+        .module('eveApp', [])
+        .controller('WelcomeController', ['$scope', function ($scope) {
+            $scope.message = 'Welcome';
+        }])
+        .controller('ChatController', ['$scope', 'ApiAIService', function ($scope, ApiAIService) {
+            $scope.input = '';
+            $scope.messages = [];
+            $scope.speechOn = false;
 
-      ApiAIService.apiAi.onResults = function (data) {
-          console.log("> ON RESULT", data);
+            ApiAIService.apiAi.onResults = function (data) {
+                console.log("> ON RESULT", data);
 
-          var status = data.status,
-              code,
-              speech;
+                var status = data.status,
+                    code,
+                    speech;
 
-          if (!(status && (code = status.code) && isFinite(parseFloat(code)) && code < 300 && code > 199)) {
-              return;
-          }
-          ApiAIService.apiAiTts.tts(data.result.fulfillment.speech, undefined, 'en-US');
-          data.botUser  = true;
-          data.realUser = false;
-          data.eve      = Math.floor((Math.random() * 8 + 1));
-
-          $scope.messages.push(data);
-
-          $scope.input = '';
-
-          $scope.$apply();
-        };
-
-      $scope.send = function(input) {
-        console.log('send clicked');
-
-        ApiAIService.sendJson(input);
-
-        $scope.messages.push({
-          "timestamp": new Date(),
-          "result": {
-            "fulfillment": {
-              "messages": [
-                {
-                  "type": 0,
-                  "speech": input
+                if (!(status && (code = status.code) && isFinite(parseFloat(code)) && code < 300 && code > 199)) {
+                    return;
                 }
-              ]
-            }
-          },
-          "botUser": false,
-          "realUser": true
-        });
-      };
 
-    }]);
+                if ($scope.speechOn)
+                    ApiAIService.apiAiTts.tts(data.result.fulfillment.speech, undefined, 'en-US');
+
+                data.botUser = true;
+                data.realUser = false;
+                data.eve = Math.floor((Math.random() * 8 + 1));
+
+                $scope.messages.push(data);
+
+                $scope.input = '';
+
+                $scope.$apply();
+            };
+
+            $scope.send = function (input) {
+                console.log('send clicked');
+
+                ApiAIService.sendJson(input);
+
+                $scope.messages.push({
+                    "timestamp": new Date(),
+                    "result": {
+                        "fulfillment": {
+                            "messages": [
+                                {
+                                    "type": 0,
+                                    "speech": input
+                                }
+                            ]
+                        }
+                    },
+                    "botUser": false,
+                    "realUser": true
+                });
+            };
+
+        }]);
 })();
